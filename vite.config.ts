e7f44d07c22def68699f92fbf1e87e4fe1481f67
Michael Black <michaelpblack16@gmail.com>
2025-12-13 19:08:25 -0600

    fix path again

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === "production" ? "/" : "/",
+  base: mode === "production" ? "/michaelblack/" : "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
